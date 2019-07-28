5388609f408db7366d54bb493d8343cd923ac0ce
Venelin Efremov <ghent360@iqury.us>
2019-07-27 18:48:50 -0700

    Fix wrong write to PWM fan pin.

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -693,7 +693,7 @@ int16_t Temperature::getHeaterPower(const heater_ind_t heater_id) {
       if (PWM_PIN(P##_AUTO_FAN_PIN) && A < 255)          \
         analogWrite(pin_t(P##_AUTO_FAN_PIN), D ? A : 0); \
       else                                               \
-        WRITE(P##_AUTO_FAN_PIN, D);                      \
+        digitalWrite(P##_AUTO_FAN_PIN, D);               \
     }while(0)
 
     uint8_t fanDone = 0;
