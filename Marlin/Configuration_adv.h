eba753a1f3a6ec78770efdae6042c25c4fa0d9e5
Venelin Efremov <ghent360@iqury.us>
2020-11-29 09:59:08 -0800

    Switch to TMC2209 drivers.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2297,7 +2297,7 @@
     #define X_CURRENT       600        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
-    #define X_RSENSE          0.051
+    #define X_RSENSE          0.091
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
     //#define X_INTERPOLATE  true      // Enable to override 'INTERPOLATE' for the X axis
   #endif
@@ -2315,7 +2315,7 @@
     #define Y_CURRENT       600
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
-    #define Y_RSENSE          0.051
+    #define Y_RSENSE          0.091
     #define Y_CHAIN_POS      -1
     //#define Y_INTERPOLATE  true
   #endif
@@ -2333,7 +2333,7 @@
     #define Z_CURRENT        600
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
-    #define Z_RSENSE          0.051
+    #define Z_RSENSE          0.091
     #define Z_CHAIN_POS      -1
     //#define Z_INTERPOLATE  true
   #endif
@@ -2368,7 +2368,7 @@
   #if AXIS_IS_TMC(E0)
     #define E0_CURRENT      600
     #define E0_MICROSTEPS    16
-    #define E0_RSENSE         0.051
+    #define E0_RSENSE         0.091
     #define E0_CHAIN_POS     -1
     //#define E0_INTERPOLATE true
   #endif
