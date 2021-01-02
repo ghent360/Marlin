f297eb54cdf1bc1596a56ee69e7e2c00e57a47cb
Venelin Efremov <ghent360@iqury.us>
2021-01-02 04:06:08 -0800

    Adjust the Tronxy configuration.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1126,7 +1126,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
 #define INVERT_Y_DIR true
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true
 
 // @section extruder
 
@@ -1161,7 +1161,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 330
-#define Y_BED_SIZE 320
+#define Y_BED_SIZE 275
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
