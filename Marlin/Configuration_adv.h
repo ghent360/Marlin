f07cfe940a7e042873035ca16bd54542e0c829cf
Venelin Efremov <ghent360@iqury.us>
2021-01-02 23:56:20 -0800

    Increase Extruder current to 900mA.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2397,7 +2397,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      600
+    #define E0_CURRENT      900
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.091
     #define E0_CHAIN_POS     -1
